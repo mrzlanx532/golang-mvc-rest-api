5f026b212e398a59b2b2b1485eadf60e81422ac8
Denis <denis_danilov_97@mail.ru>
2022-08-08 20:24:33 +0300

    Temp commit

--- main.go
+++ main.go
@@ -1,15 +1,24 @@
 package main
 
 import (
+	"fmt"
+	"os"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+
+	godotenv.Load()
+
 	router := gin.Default()
 	router.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
-	router.Run()
+
+	fmt.Println("test")
+
+	router.Run(":"+os.Getenv("PORT"))
 }
