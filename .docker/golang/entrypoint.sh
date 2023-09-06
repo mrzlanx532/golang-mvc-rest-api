3f3203a1cadc5f9d8dee0c2bbac2e72fb6bdd3bb
Denis Danilov <d.danilov@duotek.ru>
2023-09-06 16:56:08 +0300

    Refactoring

--- .docker/golang/entrypoint.sh
+++ .docker/golang/entrypoint.sh
@@ -3,7 +3,7 @@
 while true
 do 
     ANY_ANSWER=$(migrate -source file:./database/migrations -database mysql://golang_rest_api:golang_rest_api@tcp\(mysql:3306\)/golang_rest_api up 2>&1)
-    echo $ANY_ANSWER
+    echo "$ANY_ANSWER"
 
     if [[ ! $ANY_ANSWER =~ "connection refused" ]]; then
         break
@@ -11,4 +11,6 @@ do
     sleep 5
 done
 
-go run /usr/src/app/cmd/app/main.go
+go build -o /usr/src/app/cmd/api/main /usr/src/app/cmd/api/main.go
+
+/usr/src/app/cmd/api/main
